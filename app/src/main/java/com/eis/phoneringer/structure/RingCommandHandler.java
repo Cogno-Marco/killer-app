50963587c6c0589ddb224a889a4e87d72b2798c4
Alberto Ursino <alberto.ursino@studenti.unipd.it>
2019-11-27 18:19:40 +0100

    Created RingCommandHandler class (#8)
    * Created the RingCommandHandler class
    
    * Removed empty spaces
    
    * Fixed singleton
    
    * Applied the changes proposed by Luca Crema
    
    * Adapted the code to the new smsLibrary

---
+++ app/src/main/java/com/eis/phoneringer/structure/RingCommandHandler.java
@@ -0,0 +1,60 @@
+package com.eis.phoneringer.structure;
+
+import com.eis.smslibrary.SMSMessage;
+
+/**
+ * Class used to parse RingCommand to SMSMessage and back *
+ *
+ * @author Alberto Ursino, Luca Crema, Marco Mariotto
+ */
+public class RingCommandHandler {
+
+    public static final String SPLIT_CHARACTER = "_";
+
+    /**
+     * Instance of the class that is instantiated in getInstance method
+     */
+    private static RingCommandHandler instance = null;
+
+    /**
+     * Private constructor
+     */
+    private RingCommandHandler() {
+    }
+
+    /**
+     * @return the RingCommandHandler instance
+     */
+    public static RingCommandHandler getInstance() {
+        if (instance == null)
+            instance = new RingCommandHandler();
+        return instance;
+    }
+
+    /**
+     * Extracts the password from the message received and create a RingCommand
+     * A valid content is the following: "_password"
+     *
+     * @param smsMessage to parse
+     * @return a RingCommand object, null if the message doesn't contain a valid one
+     */
+    public RingCommand parseMessage(SMSMessage smsMessage) {
+        if ((smsMessage.getData().charAt(0) + "").equals(SPLIT_CHARACTER)) {
+            String[] parts = smsMessage.getData().split(SPLIT_CHARACTER);
+            //parts[0] is empty, parts[1] contains the password
+            return new RingCommand(smsMessage.getPeer(), parts[1]);
+        }
+        return null;
+    }
+
+    /**
+     * Extracts the password and the peer from the RingCommand and creates a SMSMessage object
+     *
+     * @param ringCommand to parse, it must be a valid one
+     * @return a SMSMessage object
+     */
+    public SMSMessage parseCommand(RingCommand ringCommand) {
+        return new SMSMessage(ringCommand.getPeer(), ringCommand.getPassword());
+    }
+
+}
