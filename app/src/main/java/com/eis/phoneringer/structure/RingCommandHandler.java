da0c859b2354f01a97f9e0e147e42402258742be
Alberto Ursino <alberto.ursino@studenti.unipd.it>
2019-11-27 17:54:33 +0100

    Applied the changes proposed by Luca Crema

--- app/src/main/java/com/eis/phoneringer/structure/RingCommandHandler.java
+++ app/src/main/java/com/eis/phoneringer/structure/RingCommandHandler.java
@@ -1,9 +1,12 @@
 package com.eis.phoneringer.structure;
 
+import android.content.Context;
+
+import com.eis.smslibrary.SMSMessage;
 import com.eis.smslibrary.SMSPeer;
 
 /**
- * Class used to check if password in memory corresponds to the password passed through sender
+ * Class used to parse RingCommand to SMSMessage and back *
  *
  * @author Alberto Ursino, Luca Crema, Marco Mariotto
  */
@@ -26,26 +29,36 @@ public class RingCommandHandler {
      * @return the RingCommandHandler instance
      */
     public static RingCommandHandler getInstance() {
-        if(instance == null)
+        if (instance == null)
             instance = new RingCommandHandler();
         return instance;
     }
 
     /**
-     * Extracts the password from the message received and create a RingCommand, null if the command is not valid
+     * Extracts the password from the message received and create a RingCommand
      * A valid content is the following: "_password"
      *
-     * @param peer    is the sender/receiver of the message
-     * @param content command
-     * @return a RingCommand
+     * @param smsMessage to parse
+     * @return a RingCommand object, null if the message doesn't contain a valid one
      */
-    public RingCommand parseContent(SMSPeer peer, String content) {
-        if ((content.charAt(0) + "").equals(SPLIT_CHARACTER)) {
-            String[] parts = content.split(SPLIT_CHARACTER);
+    public RingCommand parseMessage(SMSMessage smsMessage) {
+        if ((smsMessage.getData().charAt(0) + "").equals(SPLIT_CHARACTER)) {
+            String[] parts = smsMessage.getData().split(SPLIT_CHARACTER);
             //parts[0] is empty, parts[1] contains the password
-            return new RingCommand(peer, parts[1]);
+            return new RingCommand(smsMessage.getPeer(), parts[1]);
         }
         return null;
+    }
+
+    /**
+     * Extracts the password and the peer from the RingCommand and creates a SMSMessage object
+     *
+     * @param ringCommand to parse, it must be a valid one
+     * @param context     of the application
+     * @return a SMSMessage object
+     */
+    public SMSMessage parseCommand(Context context, RingCommand ringCommand) {
+        return new SMSMessage(context, ringCommand.getPeer().toString(), ringCommand.getPassword());
     }
 
 }
--- app/src/test/java/com/eis/phoneringer/structure/RingCommandHandlerTest.java
+++ app/src/test/java/com/eis/phoneringer/structure/RingCommandHandlerTest.java
@@ -1,5 +1,6 @@
 package com.eis.phoneringer.structure;
 
+import com.eis.smslibrary.SMSMessage;
 import com.eis.smslibrary.SMSPeer;
 
 import org.junit.Assert;
@@ -13,13 +14,16 @@ import org.junit.Test;
  */
 public class RingCommandHandlerTest {
 
-    private static final String SPLIT_CHARACTER = "_";
+    private static final String SPLIT_CHARACTER = RingCommandHandler.SPLIT_CHARACTER;
     private static final String VALID_NUMBER = "+391111111111";
     private static final String VALID_PASSWORD = "pass";
     private static final String VALID_CONTENT = SPLIT_CHARACTER + VALID_PASSWORD;
     private static final String WRONG_CONTENT = VALID_PASSWORD;
+    private static final int VALID_APP_ID = 1;
     private static SMSPeer SMS_PEER = new SMSPeer(VALID_NUMBER);
     private RingCommandHandler ringCommandHandler = null;
+    private SMSMessage smsMessage = new SMSMessage(new SMSPeer(VALID_NUMBER), VALID_CONTENT);
+
 
     @Before
     public void init() {
@@ -28,22 +32,22 @@ public class RingCommandHandlerTest {
 
     @Test
     public void parseContent_content_isValid() {
-        Assert.assertNotEquals(null, ringCommandHandler.parseContent(SMS_PEER, VALID_CONTENT));
+        Assert.assertNotEquals(null, ringCommandHandler.parseMessage(smsMessage));
     }
 
     @Test
     public void parseContent_content_isNotValid() {
-        Assert.assertEquals(null, ringCommandHandler.parseContent(SMS_PEER, WRONG_CONTENT));
+        Assert.assertEquals(null, ringCommandHandler.parseMessage(new SMSMessage(VALID_APP_ID,VALID_NUMBER, WRONG_CONTENT)));
     }
 
     @Test
     public void parseContent_ringCommandPasswords_areEquals() {
-        Assert.assertEquals(new RingCommand(SMS_PEER, VALID_PASSWORD).getPassword(), ringCommandHandler.parseContent(SMS_PEER, VALID_CONTENT).getPassword());
+        Assert.assertEquals(new RingCommand(SMS_PEER, VALID_PASSWORD).getPassword(), ringCommandHandler.parseMessage(smsMessage).getPassword());
     }
 
     @Test
     public void parseContent_ringCommandPeers_areEquals() {
-        Assert.assertEquals(new RingCommand(SMS_PEER, VALID_CONTENT).getPeer(), ringCommandHandler.parseContent(SMS_PEER, VALID_CONTENT).getPeer());
+        Assert.assertEquals(new RingCommand(SMS_PEER, VALID_CONTENT).getPeer(), ringCommandHandler.parseMessage(smsMessage).getPeer());
     }
 
 }
