9768ac394498cee6dc557f3f0deb31649c3c72ac
Alberto Ursino <alberto.ursino@studenti.unipd.it>
2019-11-30 00:14:50 +0100

    File Removed
    Useless file.
    I forgot to take it off.

--- app/src/main/java/com/eis/phoneringer/exceptions/IllegalCommandException.java
+++
@@ -1,9 +0,0 @@
-package com.eis.phoneringer.exceptions;
-
-/**
- * Usually thrown when the received command cannot be parsed correctly
- *
- * @author Alberto Ursino
- */
-public class IllegalCommandException extends RuntimeException {
-}
